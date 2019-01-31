49e13c6efcb4391c07bd95da98228e927399e554
Anton_azarenka <anton_azarenka@epam.com>
2019-01-31 09:55:22 +0300

    fix version 0.7.2

--- src/app/entity/prop.ts
+++ src/app/entity/prop.ts
@@ -1,3 +1,4 @@
 export class Prop{
  public static URI: string = 'https://back-end-furniture.herokuapp.com/furniture/order/';
+ public static version = '0.7.3'
 }
--- src/app/ui/footer/footer.component.ts
+++ src/app/ui/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Prop} from "../../entity/prop";
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public  version: string;
   constructor() { }
 
   ngOnInit() {
+    this.version = Prop.version;
   }
 
 }
