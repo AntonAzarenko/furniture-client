6775960b37533b5534c73625a8cbe98a948de4c9
Anton_azarenka <anton_azarenka@epam.com>
2019-02-21 18:09:16 +0300

    version 0.7.4

--- src/app/properties.ts
+++ src/app/properties.ts
@@ -3,5 +3,5 @@ export class Prop{
  //public static URI: string = 'https://back-end-furniture.herokuapp.com';
 // public static URI: string = 'http://localhost:8080';
 
- public static version = '0.7.3'
+ public static version = '0.7.4'
 }
